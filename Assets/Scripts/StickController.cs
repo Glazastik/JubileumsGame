f459bcb8c05d136106ec1428849e6bee135131fc
Glazastik <anton.myrholm@gmail.com>
2016-05-06 21:18:11 +0200

    begun working animation

---
+++ Assets/Scripts/StickController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StickController : MonoBehaviour {
+
+    private Animator anim;
+    private string direction = "right";
+    private Rigidbody2D rb2d;
+    public int speed;
+	// Use this for initialization
+	void Start () {
+        anim = this.GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement.normalized * speed * Time.fixedDeltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(new Vector2(0, 100));
+        }
+	}
+}
