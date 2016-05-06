c74484ce9fa9808c517030cb5fcc79d0813dadc2
Glazastik <anton.myrholm@gmail.com>
2016-05-06 19:33:04 +0200

    no more fly :(

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour {
 	
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(moveHorizontal, 0);
