4ffca812205a199b7e0aec9f791ad65ffba2b53c
Viktor Karlsson <wiixtor@gmail.com>
2016-05-07 23:55:06 +0200

    more scene fix

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class BlockSpawner : MonoBehaviour {
 
-    public GameObject player;
+    public GameObject[] player;
     public Transform[] block;
     private Transform chosenBlock;
     public int initSpawnDelay;
@@ -25,7 +25,7 @@ public class BlockSpawner : MonoBehaviour {
         {
             xOffset = Random.Range(-7, 7);
             randomBlock = Random.Range(0, block.Length);
-            Instantiate(block[randomBlock], new Vector3(xOffset, player.transform.position.y + 10), Quaternion.identity);
+            Instantiate(block[randomBlock], new Vector3(xOffset, player[0].transform.position.y + 10), Quaternion.identity);
             spawnTic = 0;
             if (spawnDelay >= 10)
             {
