84b70e37b6c333ceca8ab099709354fc574cbac4
Glazastik <anton.myrholm@gmail.com>
2016-05-07 23:08:11 +0200

    Fixed camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,19 +4,20 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
     public Transform[] players;
     private Vector3 pos;
+    private Camera cam;
 	// Use this for initialization
 	void Start () {
-	
+        cam = GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        pos = Camera.main.WorldToViewportPoint(players[0].position);
+        pos = cam.WorldToViewportPoint(players[0].position);
         if (pos.y > 1)
         {
-            var camTrans = Camera.current.transform;
+            var camTrans = cam.transform;
             camTrans.position = new Vector3(camTrans.position.x, camTrans.position.y + 1, camTrans.position.z);
-            Camera.main.transform.position = camTrans.position;
+            cam.transform.position = camTrans.position;
         }
 	}
 }
