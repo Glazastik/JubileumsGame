a40701d2ffc96a0fb90d9199736f2fe4d09dfef2
Viktor Karlsson <wiixtor@gmail.com>
2016-05-07 23:19:48 +0200

    block spawn relative to player

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class BlockSpawner : MonoBehaviour {
 
+    public GameObject player;
     public Transform[] block;
     private Transform chosenBlock;
     public int initSpawnDelay;
@@ -24,7 +25,7 @@ public class BlockSpawner : MonoBehaviour {
         {
             xOffset = Random.Range(-7, 7);
             randomBlock = Random.Range(0, block.Length);
-            Instantiate(block[randomBlock], new Vector3(xOffset, 10), Quaternion.identity);
+            Instantiate(block[randomBlock], new Vector3(xOffset, player.transform.position.y + 20), Quaternion.identity);
             spawnTic = 0;
             if (spawnDelay >= 10)
             {
